3a3b7023f61ed1f5ebaba7d5c899c320aa1787fa
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-16 17:13:31 -0500

    my last commit showed no changes

--- index.php
+++ index.php
@@ -4,4 +4,8 @@ echo "My name is Khadejah Hylton v2";
 echo"<br>";
 echo "Boom!! We are Live!!";
 
+
+echo "If you view the page source \r\n you will find a newline in this string.";
+echo "<br>";
+echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
 ?>
