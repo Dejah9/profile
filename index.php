2fb810e1d2b6650bfb78171c264d801e1b04901c
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-17 12:47:42 -0500

    add words to about and upgraded nav bar with colour and pushed it to the right... ish.

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
  </section>
  <!-- User Info Section -->
  <section id="user-info">
- 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 	<nav class="navbar navbar-expand-lg">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -65,6 +65,11 @@
 	<div class="row">
 		<div class="col-md-6 text-center">
 			<img src="kabrina.jpg" class="profile-img">
+		</div>
+		<div class="col-md-6">
+			<h3>WHO AM I?</h3>
+			<P>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</P>
+			
 		</div>
 	</div>
 	
