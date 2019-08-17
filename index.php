fcd60b11c6d160a176fb40f928ba11ecf777f878
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-17 09:53:18 -0500

    test html

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<html> 
+ <title>HTML with PHP</title>
+ <body>
+ <h1>My Example</h1>
+ <?php
+//your PHP code goes here
+?>
+ <b>Here is some more HTML</b>
+ <?php
+ //more PHP code
+ ?>
+ 
+ </body>
+
+
 <?php
 
 echo "My name is Khadejah Hylton v2";
