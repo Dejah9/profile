b2c2e4fb713a320024305eee8293723d18d19856
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-17 12:14:54 -0500

    ADDED A MENU

--- index.php
+++ index.php
@@ -32,6 +32,35 @@
  		<div class="scroll-bar"><a href=""><span></span></a></div>
  	</div>
  </section>
+ <!-- User Info Section -->
+ <section id="user-info">
+ 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT ME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">RESUME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">SERVICES</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+ </section>
+
  
  </body>
 
