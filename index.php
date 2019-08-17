89c911ed06aa257906c159c7a1311543679d6043
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-17 10:22:47 -0500

    added css page and pic

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <html> 
- <title>HTML with PHP</title>
+<head>
+	 <title>Resume Website</title>
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
  <body>
- <h1>My Example</h1>
+<!--  <h1>My Example</h1>
  <?php
 //your PHP code goes here
 ?>
  <b>Here is some more HTML</b>
  <?php
  //more PHP code
- ?>
+ ?> -->
+
+ <!-- Header Section -->
+ <section id="header">
+ 	<div class="container text-center">
+ 		<div class="user-box">
+ 			<img src="khadejah1.jpg">
+ 		</div>
+ 	</div>
+ </section>
  
  </body>
 
