4d5907e4d69a594d15daf63732bb552f1ae63f66
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-18 21:45:37 -0500

    Skills bar black

--- index.php
+++ index.php
@@ -74,32 +74,31 @@
 			<h3>WHO AM I?</h3>
 			<P>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</P>
 			<div class="skills-bar">
+				
 				<p>Adobe Photoshop</p>
 				<div class="progress">
-				<div class="progress-bar" style="width: 80% "> 80%</div>
-			</div>
+					<div class="progress-bar" style="width: 80% "> 80%</div>
+				</div>
 
 				<p>Adobe Photoshop</p>
 				<div class="progress">
-				<div class="progress-bar" style="width: 80% "> 80%</div>
-			</div>
+					<div class="progress-bar" style="width: 80% "> 80%</div>
+				</div>
 
 				<p>UI Design</p>
 				<div class="progress">
-				<div class="progress-bar" style="width: 80% "> 80%</div>
-			</div>
+					<div class="progress-bar" style="width: 80% "> 80%</div>
+				</div>
 
 				<p>Wordpress</p>
 				<div class="progress">
-				<div class="progress-bar" style="width: 80% "> 80%</div>
-			</div>
+					<div class="progress-bar" style="width: 80% "> 80%</div>
+				</div>
 
 				<p>Graphics Design</p>
 				<div class="progress">
-				<div class="progress-bar" style="width: 80% "> 80%</div>
-			</div>
-
-
+					<div class="progress-bar" style="width: 80% "> 80%</div>
+				</div>
 			</div>
 			</div>
 			
