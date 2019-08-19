2281377b0b36391aa4a9b8e08d6f4df533d12b85
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-18 21:32:04 -0500

    Skills added

--- index.php
+++ index.php
@@ -73,6 +73,30 @@
 		<div class="col-md-6">
 			<h3>WHO AM I?</h3>
 			<P>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</P>
+			<div class="skills-bar">
+				<p>Adobe Photoshop</p>
+			<div class="progress">
+				<div class="progress-bar" style="width: 80% "> 80%</div>
+
+				<p>Adobe Photoshop</p>
+			<div class="progress">
+				<div class="progress-bar" style="width: 80% "> 80%</div>
+
+				<p>UI Design</p>
+			<div class="progress">
+				<div class="progress-bar" style="width: 80% "> 80%</div>
+
+				<p>Wordpress</p>
+			<div class="progress">
+				<div class="progress-bar" style="width: 80% "> 80%</div>
+
+				<p>Graphics Design</p>
+			<div class="progress">
+				<div class="progress-bar" style="width: 80% "> 80%</div>
+
+
+			</div>
+			</div>
 			
 		</div>
 	</div>
