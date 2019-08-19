40985d7aa600f37fd2f4d9612d2b9dd5e42968bc
Khadejah Hylton <Khadejah.Hylton@jcaa.gov.jm>
2019-08-18 22:13:51 -0500

    testing

--- index.php
+++ index.php
@@ -10,14 +10,6 @@
 
 </head>
  <body>
-<!--  <h1>My Example</h1>
- <?php
-//your PHP code goes here
-?>
- <b>Here is some more HTML</b>
- <?php
- //more PHP code
- ?> -->
 
  <!-- Header Section -->
  <section id="header">
@@ -150,16 +142,4 @@
 
  
  </body>
-
-
-<?php
-
-echo "My name is Khadejah Hylton v2";
-echo"<br>";
-echo "Boom!! We are Live!!";
-
-
-echo "If you view the page source \r\n you will find a newline in this string.";
-echo "<br>";
-echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
-?>
+ </html>
